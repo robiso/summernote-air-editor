8475c2b94c24a93d29f9c5f11b432fd6095764b6
Stephan Stanisic <stephanstanisic@gmail.com>
2019-08-16 10:09:03 +0200

    Added 'more' button

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,39 @@
 $(function() {
-    var editElements = {};
+    var extendedToolbarVisible = false,
+        MoreButton = function (context) {
+            var ui = $.summernote.ui;
+
+            // create button
+            var button = ui.button({
+                contents: '<i class="fa fa-angle-right"/>',
+                click: function () {
+                    // invoke insertText method with 'hello' on editor module.
+                    if(extendedToolbarVisible) {
+                        $(".btn-group.note-color").hide();
+                        $(".btn-group.note-para").hide();
+                        $(".btn-group.note-insert button[data-original-title='Document']").hide();
+                        $(".btn-group.note-insert button[data-original-title='Video']").hide();
+                        $(".btn-group.note-table").hide();
+                        $(".btn-group.note-misc").hide();
+
+                        $(".btn-group.note-show button i").removeClass("fa-angle-left").addClass("fa-angle-right");
+                    } else {
+                        $(".btn-group.note-color").show();
+                        $(".btn-group.note-para").show();
+                        $(".btn-group.note-insert button[data-original-title='Document']").show();
+                        $(".btn-group.note-insert button[data-original-title='Video']").show();
+                        $(".btn-group.note-table").show();
+                        $(".btn-group.note-misc").show();
+
+                        $(".btn-group.note-show button i").removeClass("fa-angle-right").addClass("fa-angle-left");
+                    }
+                    extendedToolbarVisible = !extendedToolbarVisible;
+                }
+            });
+
+            return button.render();   // return button as jquery object
+        },
+        editElements = {};
     $('.editable').summernote({
         airMode: true,
         toolbar: [
@@ -12,18 +46,31 @@ $(function() {
             ['table', ['table']],
             ['misc', ['codeview']],
         ],
+        buttons: {
+            more: MoreButton
+        },
         popover: {
             air: [
-                ['style', ['style']],
+                ['style', ['style', 'fontsize']],
                 ['color', ['color']],
-                ['font', ['bold', 'italic']],
+                ['font', ['bold', 'italic', 'underline', 'clear']],
                 ['para', ['ul', 'paragraph']],
-                ['insert', ['link','image', 'doc', 'video']],
-                ['table', ['table']]
+                ['insert', ['link', 'image', 'doc', 'video']],
+                ['table', ['table']],
+                ['misc', ['codeview']],
+                ['show', ['more']]
             ]
         },
         placeholder: 'Click here to enter content.',
         callbacks: {
+            onInit: function() {
+                $(".btn-group.note-color").hide();
+                $(".btn-group.note-para").hide();
+                $(".btn-group.note-insert button[data-original-title='Document']").hide();
+                $(".btn-group.note-insert button[data-original-title='Video']").hide();
+                $(".btn-group.note-table").hide();
+                $(".btn-group.note-misc").hide();
+            },
             onChange: function(contents, $editable) {
                 editElements[$(this).attr('id')] = contents;
             },
@@ -41,7 +88,7 @@ $(function() {
                     })
                     .done(function() {
                         $("#save").show();
-                        $('#save').delay(100).fadeOut();
+                        $('#save').delay(100).hide();
                     });
                 }
             }
